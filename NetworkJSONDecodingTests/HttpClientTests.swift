1f0b74c2dec409cf35d45cb19b9b27ecb2dd525b
Ikmal Azman <opikmalazman@gmail.com>
2023-04-09 08:01:16 +0100

    adding test case to verify data and error are exist

--- NetworkJSONDecodingTests/HttpClientTests.swift
+++ NetworkJSONDecodingTests/HttpClientTests.swift
@@ -1,9 +1,67 @@
 @testable import NetworkJSONDecoding
 import XCTest
 
-final class HttpClientTests: XCTestCase {}
+final class HttpClientTests: XCTestCase {
+    var sut : HttpClient!
+    let session = MockURLSession()
+    
+    override func setUp() {
+        super.setUp()
+        sut = HttpClient(session: session)
+    }
+    
+    override func tearDown() {
+        sut = nil
+        super.tearDown()
+    }
+    
+    func test_download_withData_shouldReturnData() {
+        let expectation = expectation(description: "Test Download")
+        let request = makeUserURLRequest()
+        session.data = makeMockUserData()
+        
+        sut.downloadData(session, withURL: request) { result in
+            switch result {
+            case .success(let success):
+                XCTAssertEqual(success, self.makeMockUserData())
+            case .failure(_):
+                XCTFail("Download failed where it should not to")
+            }
+            expectation.fulfill()
+        }
+        
+        wait(for: [expectation], timeout: 0.01)
+    }
+    
+    func test_download_withError_shouldNotReturnData() {
+        let expectation = expectation(description: "Test Download")
+        let request = makeUserURLRequest()
+        let error = NSError(domain: "Error download data", code: 0)
+        session.error = error
+        
+        sut.downloadData(session, withURL: request) { result in
+            switch result {
+            case .success(let success):
+                XCTFail("Download success where it should not to")
+            case .failure(let error):
+                XCTAssertNotNil(error)
+            }
+            expectation.fulfill()
+        }
+        
+        wait(for: [expectation], timeout: 0.01)
+    }
+}
 
 private extension HttpClientTests {
+    func makeUserURLRequest(_ file : StaticString = #file, _ line : UInt = #line) -> URLRequest {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/") else {
+            fatalError("Invalid URL", file: file, line: line)
+        }
+        let request = URLRequest(url: url)
+        return request
+    }
+    
     func makeMockUserData() -> Data {
         let data =
     """
--- NetworkJSONDecodingTests/MockURLSession.swift
+++ NetworkJSONDecodingTests/MockURLSession.swift
@@ -10,16 +10,32 @@ import Foundation
 
 final class MockURLSession : URLSessionContract {
     var capturedURLRequest : URL?
-    var capturedCompletion : ((Data?, URLResponse?, Error?) -> Void)?
-    let dataTask = FakeDataTask()
+    var data : Data?
+    var error : Error?
     
     func dataTask(with request: URLRequest, completionHandler: @escaping @Sendable (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
-        capturedURLRequest = request.url
-        capturedCompletion = completionHandler
-        return dataTask
+        self.capturedURLRequest = request.url
+        let data = self.data
+        let error = self.error
+        
+        return FakeDataTask {
+            completionHandler(data, nil, error)
+        }
     }
 }
 
+
+/// `URLSessionDataTask`, return download data to memory
 final class FakeDataTask : URLSessionDataTask {
-    override func resume() {}
+    
+    /// `closure`, completion handler that allow us supply the return data for testing
+    private  let closure : () -> Void
+    
+    init(closure: @escaping () -> Void) {
+        self.closure = closure
+    }
+    
+    override func resume() {
+        closure()
+    }
 }
